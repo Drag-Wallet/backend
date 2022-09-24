252048c76ecc117c81a52a3f94790dae7021afbf
golu7679 <golurajak811@gmail.com>
2022-09-24 17:45:35 +0530

    New: .bat and .sh files added New: .env.example added New app: auth_user New model: DragUser Update: tinku folder removed, drag folder added

--- drag/wsgi.py
+++ drag/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for tinku project.
+WSGI config for drag project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'tinku.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'drag.settings')
 
 application = get_wsgi_application()
